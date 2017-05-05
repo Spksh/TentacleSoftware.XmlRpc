0c42f9d74d641fe453572c22ca49215f03040913
Sam Wood <signalnoise@idolatry.org>
2017-05-06 11:56:23 +1200

    Add .nuspec files

--- TentacleSoftware.XmlRpc.Owin/Properties/AssemblyInfo.cs
+++ TentacleSoftware.XmlRpc.Owin/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TentacleSoftware.XmlRpc.Owin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An async streaming XML-RPC server for OWIN")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sam Wood")]
 [assembly: AssemblyProduct("TentacleSoftware.XmlRpc.Owin")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Sam Wood")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
